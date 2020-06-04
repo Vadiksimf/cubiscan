b2dc0797284b077a25a03867850121eee837ec0f
unknown <vadiksimf88@gmail.com>
2020-06-04 08:45:30 +0300

    start...

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+$(function() {
+
+    $("#video__play").click(function() {
+        $(this).parents('.video-video-box').html('<iframe width="770" height="475" src="https://www.youtube.com/embed/R2y8CytuZEA?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+    });
+});
